e93c898e7b1b786640727985fa4432d8279910ce
Ziad Ayman <ziad.ayman@talabat.com>
2025-05-22 14:14:57 +0300

    feat(): Add Dags

---
+++ dags/ITI_DAG_FACTORY/dag_factory.py
@@ -0,0 +1,143 @@
+import glob
+import itertools
+from datetime import datetime
+import re
+
+import yaml
+from airflow.decorators import dag, task_group
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import (
+    GCSToBigQueryOperator,
+)
+from cosmos.operators import DbtRunOperator
+
+from data_transfers.general_backend_export_transfer.dbt.include.constants import (
+    transfers_dbt_path,
+)
+from data_transfers.general_backend_export_transfer.dbt.include.profiles import (
+    transfers_dbt,
+)
+from data_transfers.transfers_utils.get_destination_project import (
+    get_destination_project,
+)
+from data_transfers.transfers_utils.get_where_condition_transfer_dags import (
+    get_where_condition,
+)
+from data_transfers.transfers_utils.operators.postgres_db_connector_new_v1 import (
+    PostgresDbConnectorOperator,
+)
+from data_transfers.transfers_utils.operators.transfer_metadata import TransferMetadata
+
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import (
+    PostgresToGCSOperator,
+)
+
+
+bucket_name = "talabat-labs-data-platform-airflow-maindb-exports"
+
+
+def generate_dag(dag_id, schedule, tables, default_args, config):
+    @dag(
+        dag_id=dag_id,
+        schedule_interval=schedule,
+        default_args=default_args,
+        concurrency=config.get("concurrency", 10),
+        max_active_runs=config.get("max_active_runs", 1),
+        description=config.get("description", ""),
+        catchup=False,
+    )
+    def extract_from_db():
+        @task_group("backend_gcs_bq_tasks")
+        def backend_gcs_bq_tasks():
+            for table_data in tables:
+                table_name = table_data["table_name"]
+                columns_select = table_data.get("columns", "*")
+                dataset = config.get("default_dataset", "data_platform_export")
+                source_table_schema = config.get("default_source_schema", "public")
+                db_type = config.get("default_db_type", "PostgreSQL")
+                postgres_conn_id = (
+                    config.get("default_postgres_conn_id", "default_postgres_conn"),
+                )
+
+                base_path = f"{dataset}/{table_name}/data_json/raw_data/"
+                filename_path = f"{base_path}raw_data/"
+
+                transfer_postgres_to_gcs = PostgresToGCSOperator(
+                    task_id=f"{table_name}_to_gcs",
+                    sql=f"""select {columns_select} from {source_table_schema}."{table_name}""",
+                    postgres_conn_id=postgres_conn_id,
+                    bucket=bucket_name,
+                    filename=filename_path + "{}.json",
+                    export_format="json",
+                )
+
+                load_gcs_to_bigquery = GCSToBigQueryOperator(
+                    task_id=f"From_GCS_To_Staging_{table_name}_BQ",
+                    bucket=bucket_name,
+                    source_objects=[filename_path + "*.json"],
+                    destination_project_dataset_table=f"talabat_labs.{dataset}.staging_{table_name}",
+                    source_format="NEWLINE_DELIMITED_JSON",
+                    write_disposition="WRITE_TRUNCATE",
+                )
+
+                (transfer_postgres_to_gcs >> load_gcs_to_bigquery)
+
+        backend_gcs_bq_tasks()
+
+    return extract_from_db()
+
+
+config_files = list(
+    itertools.chain(
+        glob.glob(
+            "./gcs/dags/customers_db/*.yaml",
+            recursive=True,
+        ),
+        glob.glob(
+            "./gcs/dags/orders_db/*.yaml",
+            recursive=True,
+        ),
+        glob.glob(
+            "./gcs/dags/products_db/*.yaml",
+            recursive=True,
+        ),
+    )
+)
+
+for config_path in config_files:
+    with open(config_path, "r") as stream:
+        config = yaml.safe_load(stream)
+
+    required_keys = ["dag_id", "tables"]
+    missing_keys = [key for key in required_keys if key not in config]
+    if missing_keys:
+        raise KeyError(
+            f"Required keys {missing_keys} are missing in the YAML file: {config_path}"
+        )
+
+    dag_id = config["dag_id"]
+    schedule = config.get("schedule_interval", None)
+    tables = config["tables"]
+
+    default_args = {
+        "owner": config.get("default_args", {}).get("owner", "DE team"),
+        "email": config.get("default_args", {}).get(
+            "email", ["data.eng.team@talabat.com"]
+        ),
+        "start_date": datetime.strptime(
+            config.get("default_args", {}).get("start_date", "2024-05-02"), "%Y-%m-%d"
+        ),
+        "depends_on_past": config.get("default_args", {}).get("depends_on_past", False),
+        "catchup": config.get("default_args", {}).get("catchup", False),
+        "retries": config.get("default_args", {}).get("retries", 1),
+        "email_on_failure": config.get("default_args", {}).get(
+            "email_on_failure", True
+        ),
+        "gcp_conn_id": config.get("default_args", {}).get(
+            "gcp_conn_id", "bigquery_default"
+        ),
+        "max_active_runs": config.get("default_args", {}).get("max_active_runs", 1),
+        "concurrency": config.get("default_args", {}).get("concurrency", 1),
+    }
+
+    globals()[dag_id] = generate_dag(dag_id, schedule, tables, default_args, config)
---
+++ dags/ITI_DAG_FACTORY/master_dag.py
@@ -0,0 +1,58 @@
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from datetime import datetime
+from airflow.utils.task_group import TaskGroup
+from airflow import DAG
+
+default_args = {
+    "owner": "Data Engineering",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 9, 22),
+    "catchup": False,
+    "bigquery_conn_id": "bigquery_default",
+    "max_active_runs": 1,
+    "retries": 0,
+}
+
+
+dag = DAG(
+    dag_id="daily_transfer_run",
+    description="All transfer DAGs",
+    schedule_interval="05 00 * * *",
+    concurrency=6,
+    max_active_runs=1,
+    default_args=default_args,
+    tags=["master_transfer"],
+    priority=1,
+)
+
+ext_date = "{{ execution_date }}"
+
+
+def create_trigger_task(trigger_dag_id):
+    return TriggerDagRunOperator(
+        task_id=trigger_dag_id,
+        dag=dag,
+        trigger_dag_id=trigger_dag_id,
+        execution_date=ext_date,
+        wait_for_completion=True,
+        poke_interval=30,
+        deferrable=True,
+        trigger_rule="all_done",
+    )
+
+
+with TaskGroup(group_id="P1_pipelines", dag=dag) as P1_pipelines:
+    p1_dags = [
+        "FIRST_DATABASE_DAG_ID",
+        "SECOND_DATABASE_DAG_ID",
+    ]
+    for task_id in p1_dags:
+        create_trigger_task(task_id)
+
+with TaskGroup(group_id="P2_pipelines", dag=dag) as P2_pipelines:
+    p2_dags = [
+        "THIRD_DATABASE_DAG_ID",
+        "FOURTH_DATABASE_DAG_ID",
+    ]
+    for task_id in p2_dags:
+        create_trigger_task(task_id)
