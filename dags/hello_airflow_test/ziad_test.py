6ce9d43e65acb4e8fa0fe7ff2e34fa5bb2fb1a63
Ziad Ayman <ziad.ayman@talabat.com>
2025-05-22 15:28:48 +0300

    feat(): Create ziad airflow test

---
+++ dags/hello_airflow_test/ziad_test.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+def greet():
+    print("Hello from Airflow!")
+
+
+with DAG(
+    dag_id="example_airflow_dag_ziad",
+    schedule=None,
+    start_date=datetime(2025, 5, 15),
+    catchup=False,
+    tags=["example"],
+) as dag:
+    greet_task = PythonOperator(
+        task_id="greet_task",
+        python_callable=greet,
+    )
