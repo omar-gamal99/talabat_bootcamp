2a754ad578e721f04713534b83b23510317f1445
Asmaa <asmaaibrahem0000@gmail.com>
2025-05-23 17:32:33 +0200

    asmaa

--- dags/postgress_db_dags/postgre_asmaa.py
+++ dags/postgress_db_dags/postgre_asmaa.py
@@ -26,7 +26,7 @@ with DAG(
         sql=SQL_SELECT,
         bucket=BUCKET_NAME,
         filename=FILE_NAME,
-        export_format="json",
+        export_format="JSON",
     )
 
     # Step 2: Load from GCS to BigQuery
