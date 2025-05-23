48e4fbab17fbdf34c3f4e337cc410050fa8125bc
hendatwa <hendatwa20@gmail.com>
2025-05-23 15:37:53 +0300

    first_dag

---
+++ dags/hello_airflow_test/dag_test_hend.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+def my_function():
+    print("new python dag")
+
+
+with DAG(
+    dag_id='python_dag_hend',
+    schedule_interval=datetime(2025,5,22)
+
+)as dag:
+
+    python_task = PythonOperator(
+        task_id='python_dag_hend',
+        python_callable=my_function
+    )
