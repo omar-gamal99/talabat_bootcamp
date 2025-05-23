0d79e054422c07ce3d8b5fa5260b69bdff6070f0
Islamsaydd <islamsayd007@icloud.com>
2025-05-23 18:19:11 +0300

    commiting6

--- dags/postgress_db_dags/pg_to_bq_dag2_islamsayed.py
+++ dags/postgress_db_dags/pg_to_bq_dag2_islamsayed.py
@@ -1,60 +1,60 @@
 from datetime import timedelta, datetime
-
 from airflow import DAG
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCheckOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator
 
-## Define the default arguments for the DAG
+# Default arguments
 default_args = {
     'owner': 'islamsayed',
     'start_date': datetime(2023, 5, 22),
-    'retries': 1,  # Number of retries if a task fails
-    'retry_delay': timedelta(minutes=5),  # Time between retries
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
-## Create a DAG instance
+# DAG definition
 dag = DAG(
     dag_id='bigquery_etl_islamsayed',
     default_args=default_args,
     description='An Airflow DAG for IDX Stock Postgre to BigQuery',
-    schedule_interval=None,  # Set the schedule interval (e.g., None for manual runs)
-    catchup=True  # Do not backfill (run past dates) when starting the DAG
+    schedule_interval=None,
+    catchup=True
 )
-## BigQuery config variables
+
+# Configuration
 BQ_PROJECT = "talabat-labs-3927"
-BQ_DATASET = "talabat-labs-3927.landing"
+BQ_DATASET = "landing"
 BQ_TABLE1 = "order_data_islamsayed"
 BQ_BUCKET = 'talabat-labs-postgres-to-gcs'
 
-## Postgres config variables
-PG_CONN_ID = "postgres_connection_islamsayed" # Defined in airflow connection
+PG_CONN_ID = "postgres_connection_islamsayed"
 PG_SCHEMA = "public"
 PG_TABLE1 = "orders"
 
-## Task to transfer data from PostgreSQL to GCS
+# PostgreSQL to GCS
 postgres_to_gcs = PostgresToGCSOperator(
-    task_id = 'postgres_to_gcs',
-    sql = f'SELECT * FROM "{PG_SCHEMA}"."{PG_TABLE1}";',
-    bucket = BQ_BUCKET,
+    task_id='postgres_to_gcs',
+    sql=f'SELECT * FROM "{PG_SCHEMA}"."{PG_TABLE1}";',
+    bucket=BQ_BUCKET,
     filename='orders_islam_{{ ds }}.csv',
-    export_format = 'CSV',  # You can change the export format as needed
-    postgres_conn_id = PG_CONN_ID,  # Set your PostgreSQL connection ID
-    field_delimiter=',',  # Optional, specify field delimiter for CSV
-    gzip = False,  # Set to True if you want to compress the output file
-    task_concurrency = 1,  # Optional, adjust concurrency as needed
-    dag = dag,
+    export_format='CSV',
+    postgres_conn_id=PG_CONN_ID,
+    field_delimiter=',',
+    gzip=False,
+    dag=dag,
 )
-## Task to transfer data from GCS Bucket to BigQuery
+
+# GCS to BigQuery
 bq_stock_load_csv = GCSToBigQueryOperator(
     task_id="bq_stock_load_csv",
     bucket=BQ_BUCKET,
-    destination_project_dataset_table=f"{BQ_PROJECT}.{BQ_DATASET}.{BQ_TABLE1}",
+    source_objects=['orders_islam_{{ ds }}.csv'],
+    destination_project_dataset_table=f"{BQ_PROJECT}:{BQ_DATASET}.{BQ_TABLE1}",
     source_format="CSV",
-    source_uris=['gs://talabat-labs-postgres-to-gcs/orders_islam_{{ ds }}.csv'],  # Corrected
     create_disposition='CREATE_IF_NEEDED',
     write_disposition="WRITE_TRUNCATE",
+    skip_leading_rows=1,  # If your CSV has headers
     dag=dag,
 )
+
+# DAG dependencies
 postgres_to_gcs >> bq_stock_load_csv
