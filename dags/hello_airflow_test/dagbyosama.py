f197ab6cf1a6acd736192c3b84e5c1cd76d8a1fd
OSA-EL <osamaelsergany144@gmail.com>
2025-05-23 15:42:35 +0300

    feat() osama change one

---
+++ dags/hello_airflow_test/dagbyosama.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+# Define your function here or import it
+def greet():
+    print("Hello from Airflow!!!")
+
+# Define the DAG
+with DAG(
+    dag_id='greet_dag',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval='@daily',  # runs every day
+    catchup=False
+) as dag:
+
+    greet_task = PythonOperator(
+        task_id='greet_task',
+        python_callable=greet
+    )
