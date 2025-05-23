9d9a919f1840857efb75d45065f46d9eb6336a66
Rahoun <ashrafabdo04@gmail.com>
2025-05-23 15:31:46 +0300

    create dag to print hello

---
+++ dags/hello_airflow_test/AbdelrahmanRahoun-hello.py
@@ -0,0 +1,21 @@
+from airflow import DAG 
+from airflow.operators.python import pythonOperator
+from datetime import datetime
+
+def rahoun():
+    print('hello_rahoun')
+
+with DAG(
+    dag_id = 'talabat-test',
+    start_date = datetime(2025-5-23),
+    schedule = None ,
+    tags = ['example']
+
+) as dag :
+    hello_task = PythonOperator(
+        task_id="say_hello_task",
+        python_callable=rahoun,
+    )
+
+    hello_task    
+
