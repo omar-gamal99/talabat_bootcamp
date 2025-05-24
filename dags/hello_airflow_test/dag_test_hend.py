ddbb9675e54eb249d55b88ee2b2361b6e5d7ece1
hendatwa <hendatwa20@gmail.com>
2025-05-24 23:39:03 +0300

    update

--- dags/hello_airflow_test/dag_test_hend.py
+++ dags/hello_airflow_test/dag_test_hend.py
@@ -9,7 +9,8 @@ def my_function():
 
 with DAG(
     dag_id='python_dag_hend',
-    schedule_interval=datetime(2025,5,22)
+    schedule_interval=None,
+    start_date=datetime(2025, 5, 15)
 
 )as dag:
 
