ffb25c61707244c6485ba909b3254b360b656183
Hoalfatih <hodaalfatih2020@gmail.com>
2025-05-30 11:44:35 +0300

    v2

--- dags/master_dag/master_dage_hoda.py
+++ dags/master_dag/master_dage_hoda.py
@@ -3,12 +3,12 @@ from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime
 
 with DAG(
-    dag_id='master_dag',
+    dag_id='master_dag_by_hoda',
     start_date=datetime(2023, 1, 1),
     schedule_interval=None,
     catchup=False
 ) as dag:
     trigger = TriggerDagRunOperator(
         task_id='trigger_child',
-        trigger_dag_id='postgre-to-bigquery-hoda'  # must match dag_id in child_dag.py
+        trigger_dag_id='api_to_bigquery_payments_youmna'  # must match dag_id in child_dag.py
     )
