ec08856792888a29845dbf37fdcf77c6e3f2f90f
hendatwa <hendatwa20@gmail.com>
2025-05-30 15:02:48 +0300

    new_dag_trigger_another_dag

---
+++ dags/master_dag/master_dag_hend.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from datetime import datetime
+
+with DAG(
+    'trigger_dag_hend',
+    start_date=datetime(2025, 5, 30),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    trigger = TriggerDagRunOperator(
+        task_id='trigger_other_dag',
+        trigger_dag_id='postgres_to_bigquery_hend',
+    )
