6c9e949905485ce1bb3815132851fdca13279b86
Maya Emad <mayadraz@gmail.com>
2025-05-30 15:03:00 +0300

    changed dag id

--- dags/master_dag/trigger-another-dag-maya.py
+++ dags/master_dag/trigger-another-dag-maya.py
@@ -3,7 +3,7 @@ from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.utils.dates import days_ago
 
 with DAG(
-    dag_id='trigger_api_to_bq_gcp_maya',
+    dag_id='trigg_api_to_bq_gcp_maya',
     start_date=days_ago(1),
     schedule_interval=None,  # Manual only
     catchup=False,
@@ -11,7 +11,7 @@ with DAG(
 ) as dag:
 
     trigger_task = TriggerDagRunOperator(
-        task_id='trigger_api_to_bq_gcp_maya_dag',
+        task_id='trigg_api_to_bq_gcp_maya_dag',
         trigger_dag_id='api_to_bq_gcp_maya',  # 👈 Must match your actual DAG name
     )
 
