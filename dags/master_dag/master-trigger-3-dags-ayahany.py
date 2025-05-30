821a635c8c48795706ac32f7cfc51f2b91b7408f
Aya Hany <ayahhhany@gmail.com>
2025-05-30 15:49:41 +0300

    feat(): create trigger for 3 dags

---
+++ dags/master_dag/master-trigger-3-dags-ayahany.py
@@ -0,0 +1,31 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+POSTGRES_CONN_ID = "postgress-conn-aya-hany"
+## Define the default arguments for the DAG
+default_args = {
+    'owner': 'ayah',
+    'start_date': datetime(2025, 5, 23),
+    'retries': 1,  
+    'retry_delay': timedelta(minutes=2), 
+
+}
+dag_ids_to_trigger = ["customers_db_extract", "orders_db_extract", "products_db_extract"]
+
+with DAG(
+    dag_id="TRIGGER-Postgress-GCS-BQ-ayahany",
+    default_args=default_args,
+    tags=["master", "trigger"]
+) as dag:
+
+    for dag_id in dag_ids_to_trigger:
+        trigger = TriggerDagRunOperator(
+            task_id=f"TRIGGER-{dag_id}",
+            trigger_dag_id= dag_id,  
+        )
+
+    
+
+    trigger
